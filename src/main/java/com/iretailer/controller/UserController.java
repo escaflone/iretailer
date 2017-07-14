4f84729a5fd5b04580350bfe9b2291cb4b106872
wubin <wubin@zillionfortune.com>
2017-07-14 18:13:04 +0800

    修改数据库 密码

--- src/main/java/com/iretailer/controller/UserController.java
+++ src/main/java/com/iretailer/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import sun.plugin2.message.Message;
 
 import javax.servlet.http.HttpSession;
 import java.text.MessageFormat;
@@ -39,9 +40,13 @@ public class UserController {
         RestResponse<String> result = new RestResponse<>();
         try{
             User userDB = userService.queryUserByUserNameAndPassword(user);
-            httpSession.setAttribute("userId",userDB.getUserId());
+            if(userDB == null){
+                result.setCode(ErrorMessage.NOT_EXIST.getCode());
+                result.setMsg(MessageFormat.format(ErrorMessage.NOT_EXIST.getMsg(),user.getUserName()));
+            }else{
+                httpSession.setAttribute("userId",userDB.getUserId());
 
-
+            }
         }catch(Exception e){
             e.printStackTrace();
             result.setCode(ErrorMessage.SYSTEM_ERROR.getCode());
