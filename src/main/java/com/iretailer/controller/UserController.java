cab3c6adb7b13ba6bee438dba161873c7fc69517
wubin <wubin@zillionfortune.com>
2017-07-14 21:23:27 +0800

    移除不用的包

--- src/main/java/com/iretailer/controller/UserController.java
+++ src/main/java/com/iretailer/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import sun.plugin2.message.Message;
 
 import javax.servlet.http.HttpSession;
 import java.text.MessageFormat;
